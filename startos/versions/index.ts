13989b7e8f39483e8f7bc36a6a27f114d753f828
Matt Hill <mattnine@protonmail.com>
2026-05-15 21:46:07 -0600

    Release 2.4.1:6 — Instructions tab + start-sdk 1.5.1

--- startos/versions/index.ts
+++ startos/versions/index.ts
@@ -1,7 +1,7 @@
 import { VersionGraph } from '@start9labs/start-sdk'
-import { v_2_4_1_5 } from './v2.4.1.5'
+import { v_2_4_1_6 } from './v2.4.1.6'
 
 export const versionGraph = VersionGraph.of({
-  current: v_2_4_1_5,
+  current: v_2_4_1_6,
   other: [],
 })
