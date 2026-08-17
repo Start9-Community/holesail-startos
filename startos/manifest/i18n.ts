b9d9d7d30bae720128060ef17e9612770d87ed1e
helix-nine <267227783+helix-nine@users.noreply.github.com>
2026-08-16 22:49:38 -0600

    style: format the i18n, manifest, and version files with prettier

--- startos/manifest/i18n.ts
+++ startos/manifest/i18n.ts
@@ -8,11 +8,16 @@ export default {
       fr_FR: 'Tunnels P2P pour un accès instantané',
     },
     long: {
-      en_US: 'Create P2P tunnels instantly that bypass any network, firewall, NAT restrictions and expose local services over peer-to-peer tunnels. No Dynamic DNS required',
-      es_ES: 'Cree túneles P2P instantáneamente que eludan cualquier red, firewall, restricciones NAT y expongan servicios locales a través de túneles peer-to-peer. No se requiere DNS dinámico',
-      de_DE: 'Erstellen Sie sofort P2P-Tunnel, die alle Netzwerk-, Firewall- und NAT-Beschränkungen umgehen und lokale Dienste über Peer-to-Peer-Tunnel verfügbar machen. Kein dynamisches DNS erforderlich',
-      pl_PL: 'Twórz natychmiast tunele P2P, które omijają wszelkie ograniczenia sieci, zapory i NAT oraz udostępniaj lokalne usługi przez tunele peer-to-peer. Nie wymaga dynamicznego DNS',
-      fr_FR: 'Créez instantanément des tunnels P2P qui contournent toutes les restrictions réseau, pare-feu et NAT et exposez les services locaux via des tunnels pair-à-pair. Aucun DNS dynamique requis',
+      en_US:
+        'Create P2P tunnels instantly that bypass any network, firewall, NAT restrictions and expose local services over peer-to-peer tunnels. No Dynamic DNS required',
+      es_ES:
+        'Cree túneles P2P instantáneamente que eludan cualquier red, firewall, restricciones NAT y expongan servicios locales a través de túneles peer-to-peer. No se requiere DNS dinámico',
+      de_DE:
+        'Erstellen Sie sofort P2P-Tunnel, die alle Netzwerk-, Firewall- und NAT-Beschränkungen umgehen und lokale Dienste über Peer-to-Peer-Tunnel verfügbar machen. Kein dynamisches DNS erforderlich',
+      pl_PL:
+        'Twórz natychmiast tunele P2P, które omijają wszelkie ograniczenia sieci, zapory i NAT oraz udostępniaj lokalne usługi przez tunele peer-to-peer. Nie wymaga dynamicznego DNS',
+      fr_FR:
+        'Créez instantanément des tunnels P2P qui contournent toutes les restrictions réseau, pare-feu et NAT et exposez les services locaux via des tunnels pair-à-pair. Aucun DNS dynamique requis',
     },
   },
 }
